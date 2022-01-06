98de2c923f218f4b86ca221d056adbbaa0a81b04
garett95 <7934650@gmail.com>
2022-01-06 17:34:47 +0300

    Init project

---
+++ BookkeepingCons/BookkeepingCons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookkeepingCons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
